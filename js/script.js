ab49add1828d8106271a997f3ec143dcb02515f0
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-03-24 17:21:03 +0100

    Finished with for loop.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 var listSurname = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 var userSurnameInput;
+var userSurnameActivePushFlag = false;
+var userSurnameInputIndex;
 
 function userSurnamePush() {
   userSurnameInput = document.getElementById("userSurnameInput").value;
   listSurname.push(userSurnameInput);
   listSurname.sort();
+  userSurnameInputIndex = listSurname.indexOf(userSurnameInput);
+  userSurnameInputIndex++;
 }
 
 function listSurnamePull() {
@@ -15,4 +19,21 @@ function listSurnamePull() {
     document.getElementById("listSurnamePullList").innerHTML +=
       "<li>" + listSurname[i] + "</li>";
   }
+
+  console.log(userSurnameInput);
+  console.log(userSurnameInputIndex);
+
+
+  for (var i = 0; i < listSurname.length; i++) {
+    if (userSurnameInput = listSurname[i]) {
+      document.getElementById("userSurnameActivePush").innerHTML =
+        "La sua posizione nella lista è la numero: " + userSurnameInputIndex;
+        break;
+    }
+  }
+
+//   var i = 0;
+//   while ( userSurnameInput != listSurname[i]) {
+//     i++
+//   }
 }
